9100875af36b81f90cbc9b3615ce6fd35465c152
aaagius <aaagius@amazon.com>
2024-09-19 22:02:28 +0200

    fix storage recognition

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,9 +2,11 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const firstBucket = defineStorage({
   name: 'firstBucket',
-  isDefault: true,
   access: (allow) => ({
     'media/*': [allow.authenticated.to(['read', 'write', 'delete'])],
+    'public/*': [
+      allow.guest.to(['list', 'write', 'get'])
+    ]
   })
 });
 
