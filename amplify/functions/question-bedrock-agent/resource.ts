f3ce8d617d745880c61472d03aa78359ec77bf11
aaagius <aaagius@amazon.com>
2024-10-01 22:15:20 +0200

    Add chat agent

--- amplify/functions/question-bedrock-agent/resource.ts
+++ amplify/functions/question-bedrock-agent/resource.ts
@@ -2,7 +2,7 @@ import {defineFunction} from '@aws-amplify/backend';
 
 export const bedrockAgentDamageMatrix = defineFunction({
     name: 'question-bedrock-agent',
-    entry: "./handler.py",
+    entry: "./handler.ts",
     timeoutSeconds: 300,
     environment: {
     },
